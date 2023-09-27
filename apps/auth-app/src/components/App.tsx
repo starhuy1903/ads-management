4288310933a6124bbe708e9c322741add472739e
mitidevus <mitidevus@gmail.com>
2023-09-27 21:15:58 +0700

    feat: add header, login, sign up UI

--- apps/auth-app/src/components/App.tsx
+++ apps/auth-app/src/components/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
 import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        {/* <Login /> */}
+        <Signup />
+      </div>
+    </Router>
   );
 }
 
