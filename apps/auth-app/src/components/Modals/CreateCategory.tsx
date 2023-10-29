ee71b1f573441d5e361548d542b174b42cefcf10
starhuy1903 <huykst137@gmail.com>
2023-10-29 23:02:57 +0700

    feat: add devtool to debug

--- apps/auth-app/src/components/Modals/CreateCategory.tsx
+++ apps/auth-app/src/components/Modals/CreateCategory.tsx
@@ -1,4 +1,5 @@
-import { Box, TextField } from '@mui/material';
+import { DevTool } from '@hookform/devtools';
+import { Box, Stack, TextField } from '@mui/material';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import GeneralModal from './GeneralModal';
@@ -13,52 +14,51 @@ interface FormDataType {
 }
 
 function CreateCategory({ onModalClose }: CreateCategoryProps) {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    control,
-    setValue,
-    formState: { errors },
-  } = useForm<FormDataType>({
+  const { register, handleSubmit, control, formState } = useForm<FormDataType>({
+    mode: 'onBlur',
     defaultValues: {
       title: '',
       description: '',
     },
   });
+  const { errors } = formState;
   const [submitting, setSubmitting] = useState(false);
 
   console.log({ errors });
 
+  const onSubmit = (data: FormDataType) => {
+    setSubmitting(true);
+
+    setSubmitting(false);
+  };
+
   const body = (
-    <Box
-      component="form"
-      noValidate
-      autoComplete="off"
-      sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
-    >
-      <TextField
-        label="Title"
-        {...register('title', {
-          minLength: 3,
-          maxLength: 255,
-        })}
-        disabled={submitting}
-        helperText={errors.title?.message as string}
-        fullWidth
-      />
-      <TextField
-        label="Description"
-        {...register('description', {
-          minLength: 40,
-          maxLength: 2000,
-        })}
-        disabled={submitting}
-        helperText={errors.description?.message as string}
-        fullWidth
-        multiline
-        rows={3}
-      />
+    <Box component="form" autoComplete="off">
+      <Stack spacing={2}>
+        <TextField
+          label="Title"
+          {...register('title', {
+            required: 'Title is required',
+          })}
+          disabled={submitting}
+          error={!!errors.title}
+          helperText={errors.title?.message}
+          fullWidth
+        />
+        <TextField
+          label="Description"
+          {...register('description', {
+            required: 'Description is required',
+          })}
+          disabled={submitting}
+          error={!!errors.description}
+          helperText={errors.description?.message}
+          fullWidth
+          multiline
+          rows={3}
+        />
+      </Stack>
+      <DevTool control={control} />
     </Box>
   );
 
