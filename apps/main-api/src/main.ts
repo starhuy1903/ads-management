890d1c244c77b8b450d4e47092feaf7ed4fda631
NguyenLeHoangThong <nguyenlehoangthong2002@gmail.com>
2023-12-10 01:24:40 +0700

    fix: fix build [ci build]

--- apps/main-api/src/main.ts
+++ apps/main-api/src/main.ts
@@ -1,8 +1,8 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './modules/app/app.module';
 import InitFirebase from './services/firebase';
+import { Multer } from 'multer'; // cheating type, dont delete, will fix this later
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
