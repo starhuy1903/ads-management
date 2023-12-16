2291547c6241c054b69e6ebef00b250996afbdba
starhuy1903 <huykst137@gmail.com>
2023-12-16 20:15:17 +0700

    feat: navigate to report

--- apps/management-app/src/components/Common/Sidebar/AdDetail.tsx
+++ apps/management-app/src/components/Common/Sidebar/AdDetail.tsx
@@ -1,8 +1,16 @@
-import { Box, Button, IconButton, CircularProgress, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import {
+  Box,
+  Button,
+  IconButton,
+  CircularProgress,
+  Typography,
+} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function AdDetail() {
+  const navigate = useNavigate();
   const [data, setData] = useState<AdData | null>(null);
 
   useEffect(() => {
@@ -14,28 +22,39 @@ export default function AdDetail() {
         width: 100,
         quantity: 1,
         form: 'Cổ động',
-        category: 'Đất công viên'
-      })
-    })
+        category: 'Đất công viên',
+      });
+    });
   }, []);
 
   if (!data) {
     return (
-      <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
         <CircularProgress />
       </Box>
-    )
-  };
+    );
+  }
 
   return (
     <Box p={2}>
       <Box height={60} />
-      <Typography variant="h4" mb={1}>{data.title}</Typography>
-      <Typography variant="subtitle1" color="gray">{data.locationString}</Typography>
+      <Typography variant="h4" mb={1}>
+        {data.title}
+      </Typography>
+      <Typography variant="subtitle1" color="gray">
+        {data.locationString}
+      </Typography>
       <Box mt={3}>
         <Box display="flex">
           <Typography mr={1}>Kich thuoc: </Typography>
-          <Typography fontWeight={500}>{`${data.width}m x ${data.height}m`}</Typography> 
+          <Typography
+            fontWeight={500}
+          >{`${data.width}m x ${data.height}m`}</Typography>
         </Box>
         <Box display="flex">
           <Typography mr={1}>Hinh thuc:</Typography>
@@ -46,11 +65,17 @@ export default function AdDetail() {
           <Typography fontWeight={500}>{data.category}</Typography>
         </Box>
       </Box>
-      <Box display='flex' justifyContent="space-between" mt={2}>
+      <Box display="flex" justifyContent="space-between" mt={2}>
         <IconButton>
           <InfoOutlinedIcon color="primary" />
         </IconButton>
-        <Button variant="outlined" color="error">Bao cao</Button>
+        <Button
+          variant="outlined"
+          color="error"
+          onClick={() => navigate('/report')}
+        >
+          Bao cao
+        </Button>
       </Box>
     </Box>
   );
