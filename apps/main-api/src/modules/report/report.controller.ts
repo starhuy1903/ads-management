ff612ac659a1272e8f26cbc88905675f2f9c62b8
NguyenLeHoangThong <nguyenlehoangthong2002@gmail.com>
2023-12-16 15:04:40 +0700

    feat: add captcha method [ci build]

--- apps/main-api/src/modules/report/report.controller.ts
+++ apps/main-api/src/modules/report/report.controller.ts
@@ -9,7 +9,6 @@ import {
   Query,
   Res,
   UseInterceptors,
-  HttpStatus,
   ParseFilePipeBuilder,
   UploadedFiles,
 } from '@nestjs/common';
--- apps/main-api/src/modules/report/report.module.ts
+++ apps/main-api/src/modules/report/report.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { RecaptchaMiddleware } from '../../middlewares/captcha';
 
 @Module({
   controllers: [ReportController],
-  providers: [ReportService]
+  providers: [ReportService],
 })
-export class ReportModule {}
+export class ReportModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RecaptchaMiddleware)
+      .forRoutes({ path: '/reports', method: RequestMethod.POST });
+  }
+}
