211f04b8e0c60bb66c4a580593cf83bf5bcf8139
mitidevus <mitidevus@gmail.com>
2024-01-05 14:37:11 +0700

    chore: revert dev config file

--- apps/management-app/src/configurations/dev.ts
+++ apps/management-app/src/configurations/dev.ts
@@ -1,8 +1,7 @@
 import { baseConfigs } from './base';
 
 const configs = {
-  // apiUrl: 'https://ads-management-api.onrender.com/api',
-  apiUrl: 'http://localhost:8194/api',
+  apiUrl: 'https://ads-management-api.onrender.com/api',
 };
 
 export default Object.freeze({ ...baseConfigs, ...configs });
