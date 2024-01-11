8237b20659a26919465828d79ddf0714e9a41807
mitidevus <mitidevus@gmail.com>
2024-01-11 22:02:23 +0700

    feat: select reports of multiple wards

--- apps/management-app/src/components/Authenticated/Officer/LocationReportList/index.tsx
+++ apps/management-app/src/components/Authenticated/Officer/LocationReportList/index.tsx
@@ -10,9 +10,12 @@ import {
   TableRow,
 } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { useAppSelector } from '@/store';
 import CenterLoading from '@/components/Common/CenterLoading';
 import { Info, Response } from '@/components/Common/Icons';
 import { ListWrapper } from '@/components/Common/Layout/ScreenWrapper';
+import WardSelect from '@/components/Common/WardSelect';
+import { UserRole } from '@/constants/user';
 import { useGetReportsQuery } from '@/store/api/officer/reportApiSlice';
 import { Report } from '@/types/officer-management';
 import { formatDateTime } from '@/utils/datetime';
@@ -20,8 +23,12 @@ import { capitalize } from '@/utils/format-string';
 
 const titles = [
   'ID',
-  'Type',
   'Name',
+  'Address',
+  'Ward',
+  'District',
+  'Type',
+  'Reporter',
   'Email',
   'Created Time',
   'Updated Time',
@@ -30,13 +37,17 @@ const titles = [
 ];
 
 export default function LocationReportList() {
+  const role = useAppSelector((state) => state.user?.profile?.role);
+
   const [page, setPage] = useState<number>(1);
   const [reports, setReports] = useState<Report[]>([]);
+  const [wards, setWards] = useState<number[]>([]);
 
   const { data, isLoading, refetch } = useGetReportsQuery({
     page: page,
     take: 10,
     targetType: 'Location',
+    wards: wards,
   });
 
   useEffect(() => {
@@ -55,59 +66,81 @@ export default function LocationReportList() {
 
   return (
     <ListWrapper label="Location Reports">
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="location reports">
-          <TableHead>
-            <TableRow>
-              {titles.map((title) => (
-                <TableCell align="center" key={title}>
-                  {title}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {reports.length !== 0 ? (
-              reports.map((report: Report) => (
-                <TableRow key={report?.id}>
-                  <TableCell align="center">{report?.id}</TableCell>
-                  <TableCell align="center">
-                    {report?.reportType?.name}
+      <Box
+        sx={{
+          width: '100%',
+        }}
+      >
+        {role === UserRole?.DISTRICT_OFFICER && (
+          <WardSelect wards={wards} setWards={setWards} />
+        )}
+
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="location reports">
+            <TableHead>
+              <TableRow>
+                {titles.map((title) => (
+                  <TableCell align="center" key={title}>
+                    {title}
                   </TableCell>
-                  <TableCell align="center">{report?.fullName}</TableCell>
-                  <TableCell align="center">{report?.email}</TableCell>
-                  <TableCell align="center">
-                    {formatDateTime(report?.createdAt)}
-                  </TableCell>
-                  <TableCell align="center">
-                    {formatDateTime(report?.updatedAt)}
-                  </TableCell>
-                  <TableCell align="center">
-                    {capitalize(report?.status)}
-                  </TableCell>
-                  <TableCell>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        gap: 2,
-                      }}
-                    >
-                      <Info link={`/location-reports/${report?.id}`} />
-                      <Response link={`/reports/${report?.id}/response`} />
-                    </Box>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {reports.length !== 0 ? (
+                reports.map((report: Report) => (
+                  <TableRow key={report?.id}>
+                    <TableCell align="center">{report?.id}</TableCell>
+                    <TableCell align="center">
+                      {report?.location?.name}
+                    </TableCell>
+                    <TableCell align="center">
+                      {report?.location?.fullAddress}
+                    </TableCell>
+                    <TableCell align="center">
+                      {report?.location?.ward?.name}
+                    </TableCell>
+                    <TableCell align="center">
+                      {report?.location?.district?.name}
+                    </TableCell>
+                    <TableCell align="center">
+                      {report?.reportType?.name}
+                    </TableCell>
+                    <TableCell align="center">{report?.fullName}</TableCell>
+                    <TableCell align="center">{report?.email}</TableCell>
+                    <TableCell align="center">
+                      {formatDateTime(report?.createdAt)}
+                    </TableCell>
+                    <TableCell align="center">
+                      {formatDateTime(report?.updatedAt)}
+                    </TableCell>
+                    <TableCell align="center">
+                      {capitalize(report?.status)}
+                    </TableCell>
+                    <TableCell>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          gap: 2,
+                        }}
+                      >
+                        <Info link={`/location-reports/${report?.id}`} />
+                        <Response link={`/reports/${report?.id}/response`} />
+                      </Box>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell align="center" colSpan={9}>
+                    No results
                   </TableCell>
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell align="center" colSpan={9}>
-                  No results
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
 
       <Pagination
         count={data?.totalPages}
--- apps/management-app/src/components/Authenticated/Officer/PanelReportList/index.tsx
+++ apps/management-app/src/components/Authenticated/Officer/PanelReportList/index.tsx
@@ -10,6 +10,7 @@ import {
   TableRow,
 } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { useAppSelector } from '@/store';
 import CenterLoading from '@/components/Common/CenterLoading';
 import { Info, Response } from '@/components/Common/Icons';
 import { ListWrapper } from '@/components/Common/Layout/ScreenWrapper';
@@ -17,11 +18,17 @@ import { useGetReportsQuery } from '@/store/api/officer/reportApiSlice';
 import { Report } from '@/types/officer-management';
 import { formatDateTime } from '@/utils/datetime';
 import { capitalize } from '@/utils/format-string';
+import WardSelect from '@/components/Common/WardSelect';
+import { UserRole } from '@/constants/user';
 
 const titles = [
   'ID',
+  'Location Name',
+  'Address',
+  'Ward',
+  'District',
   'Type',
-  'Name',
+  'Reporter',
   'Email',
   'Created Time',
   'Updated Time',
@@ -30,14 +37,17 @@ const titles = [
 ];
 
 export default function PanelReportList() {
+  const role = useAppSelector((state) => state.user?.profile?.role);
+
   const [page, setPage] = useState<number>(1);
   const [reports, setReports] = useState<Report[]>([]);
+  const [wards, setWards] = useState<number[]>([]);
 
   const { data, isLoading, refetch } = useGetReportsQuery({
     page: page,
     take: 10,
     targetType: 'Panel',
-    typeId: 1,
+    wards: wards,
   });
 
   useEffect(() => {
@@ -56,59 +66,81 @@ export default function PanelReportList() {
 
   return (
     <ListWrapper label="Panel Reports">
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="panel reports">
-          <TableHead>
-            <TableRow>
-              {titles.map((title) => (
-                <TableCell align="center" key={title}>
-                  {title}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {reports.length !== 0 ? (
-              reports.map((report: Report) => (
-                <TableRow key={report?.id}>
-                  <TableCell align="center">{report?.id}</TableCell>
-                  <TableCell align="center">
-                    {report?.reportType?.name}
+      <Box
+        sx={{
+          width: '100%',
+        }}
+      >
+        {role === UserRole?.DISTRICT_OFFICER && (
+          <WardSelect wards={wards} setWards={setWards} />
+        )}
+
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="panel reports">
+            <TableHead>
+              <TableRow>
+                {titles.map((title) => (
+                  <TableCell align="center" key={title}>
+                    {title}
                   </TableCell>
-                  <TableCell align="center">{report?.fullName}</TableCell>
-                  <TableCell align="center">{report?.email}</TableCell>
-                  <TableCell align="center">
-                    {formatDateTime(report?.createdAt)}
-                  </TableCell>
-                  <TableCell align="center">
-                    {formatDateTime(report?.updatedAt)}
-                  </TableCell>
-                  <TableCell align="center">
-                    {capitalize(report?.status)}
-                  </TableCell>
-                  <TableCell>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        gap: 2,
-                      }}
-                    >
-                      <Info link={`/panel-reports/${report?.id}`} />
-                      <Response link={`/reports/${report?.id}/response`} />
-                    </Box>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {reports.length !== 0 ? (
+                reports.map((report: Report) => (
+                  <TableRow key={report?.id}>
+                    <TableCell align="center">{report?.id}</TableCell>
+                    <TableCell align="center">
+                      {report?.panel?.location?.name}
+                    </TableCell>
+                    <TableCell align="center">
+                      {report?.panel?.location?.fullAddress}
+                    </TableCell>
+                    <TableCell align="center">
+                      {report?.panel?.location?.ward?.name}
+                    </TableCell>
+                    <TableCell align="center">
+                      {report?.panel?.location?.district?.name}
+                    </TableCell>
+                    <TableCell align="center">
+                      {report?.reportType?.name}
+                    </TableCell>
+                    <TableCell align="center">{report?.fullName}</TableCell>
+                    <TableCell align="center">{report?.email}</TableCell>
+                    <TableCell align="center">
+                      {formatDateTime(report?.createdAt)}
+                    </TableCell>
+                    <TableCell align="center">
+                      {formatDateTime(report?.updatedAt)}
+                    </TableCell>
+                    <TableCell align="center">
+                      {capitalize(report?.status)}
+                    </TableCell>
+                    <TableCell>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          gap: 2,
+                        }}
+                      >
+                        <Info link={`/panel-reports/${report?.id}`} />
+                        <Response link={`/reports/${report?.id}/response`} />
+                      </Box>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell align="center" colSpan={9}>
+                    No results
                   </TableCell>
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell align="center" colSpan={9}>
-                  No results
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
 
       <Pagination
         count={data?.totalPages}
