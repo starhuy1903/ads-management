374add643fda53de0a51fdb1c066a995f845a4b0
starhuy1903 <huykst137@gmail.com>
2024-01-14 09:51:58 +0700

    feat: render maps on officer page

--- apps/management-app/src/components/Authenticated/Home/index.tsx
+++ apps/management-app/src/components/Authenticated/Home/index.tsx
@@ -1,9 +1,22 @@
 import { Box } from '@mui/material';
+import Maps from '@/components/Common/Maps';
+import SidebarContainer from '@/components/Common/Sidebar';
 
 export default function Home() {
   return (
-    <Box>
-      <h1>Home</h1>
+    <Box position="relative" width="100%" height="100%">
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        display="flex"
+        overflow="hidden"
+      >
+        <Maps></Maps>
+      </Box>
+      <SidebarContainer style={{ minWidth: 250, maxWidth: 300 }} />
     </Box>
   );
 }
