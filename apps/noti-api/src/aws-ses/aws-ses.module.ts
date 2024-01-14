f07158869ade56750d9375abb6aeed9aa8749623
NguyenLeHoangThong <nguyenlehoangthong2002@gmail.com>
2024-01-14 19:03:05 +0700

    chore: clean code + add scripts [ci build]

--- apps/noti-api/src/aws-ses/aws-ses.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AwsSesService } from './aws-ses.service';
-
-@Module({
-  providers: [AwsSesService],
-  exports: [AwsSesService],
-})
-export class AwsSesModule {}
--- apps/noti-api/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { promises as fs } from 'fs';
-import { join } from 'path';
-
-export const readTemplateFile = async (template: string) => {
-  const pathToTemplate = 'apps/noti-api/templates/' + template;
-  const fileData = await fs.readFile(pathToTemplate, 'utf-8');
-  return fileData;
-};
